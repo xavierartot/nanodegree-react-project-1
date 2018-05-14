f97cec4dd4ec85dadb56f0d3b309ba465463237e
xav <xavierartot@gmail.com>
2018-05-13 23:25:16 -0500

    star done

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -4,33 +4,44 @@ import star from '../icons/star.svg'
 
 
 export default class Rating extends Component {
+  randomId = () => Math.random().toString(6).substr(-8)
   render() {
-    const avg = isNaN(parseFloat(this.props.average))
+    const { average } = this.props
+    const avg = isNaN(parseFloat(average))
       ? 0
-      : [...Array(this.props.average * 2).fill(3)] // fill the array
+      : [...Array(average * 2).fill(3)] // fill the array
 
     const totalAvg = avg.length
     console.log(totalAvg, (totalAvg % 2))
 
-    const starJsx = (index, starType) => (<div key={index}>
-      <img src={starType} alt="star icon" style={{ width: '20px', height: '20px' }} /> xavi
-    </div>)
-
-    const displayStar = (avg, totalAvg) => {
-      for (let i = 0, len = avg.length; i < len; i++) {
-        if (i === totalAvg - 1) {
-          starJsx(i, starHalf)
-        } else if (i % 2) {
-          starJsx(i, star)
-        }
-      }
+    const styleStar = {
+      width: '20px', height: '20px',
     }
 
+    let starDisplay = ''
+    const avare = avg.length && avg.map((element, i) => {
+      if (i === totalAvg - 1) {
+        starDisplay = (
+          <img src={starHalf} alt="star icon" style={styleStar} />
+        )
+      } else if (i % 2 === 0) {
+        console.log('pair')
+        starDisplay = (
+          <img src={star} alt="star icon" style={styleStar} />
+        )
+      } else {
+        starDisplay = ''
+      }
+      return starDisplay = <div key={this.randomId()}>{starDisplay}</div>
+    })
+    console.log(avare)
+
     return (
-      <div style={{ display: 'flex' }}>
-        <div className="rating">
-          arrow
-          {displayStar(avg, totalAvg)}
+      <div>
+        <div className="rating" style={{ display: 'flex' }} >
+          {
+            avare
+          }
         </div>
       </div>
     )
