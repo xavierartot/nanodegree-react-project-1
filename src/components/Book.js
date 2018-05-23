229826b1bd4f97b1f073afde3e7d62544475c64b
xav <xavierartot@gmail.com>
2018-05-23 14:31:38 -0500

    bugs: change select

--- src/components/Book.js
+++ src/components/Book.js
@@ -92,7 +92,7 @@ class Book extends Component {
                                   <option value="currentlyReading">Currently Reading</option>
                                   <option value="wantToRead">Want to Read</option>
                                   <option value="read">Read</option>
-                                  <option value="none">Read</option>
+                                  <option value="none">None</option>
                                 </select>
                               </div>
                             </div>
